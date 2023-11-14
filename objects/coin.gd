4f97394925fa6953558c343ce87c3fd03cc664f5
rickdkk <r.de.klerk@pl.hanze.nl>
2023-11-14 15:01:25 +0100

    Reorganize files

--- objects/coin.gd
+++ objects/coin.gd
@@ -3,17 +3,20 @@ class_name Coin
 
 var time := 0.0
 
+@onready var audio_player := $AudioStreamPlayer as AudioStreamPlayer
 
 func _on_body_entered(body):
 	if body.has_method("collect_coin"):
 		body.collect_coin()
-		Audio.play("res://sounds/coin.ogg") # Play sound
+		audio_player.play()
+		self.visible = false
 
+		await audio_player.finished
 		call_deferred("queue_free")
 
 
 func _process(delta):
 	rotate_y(2 * delta) # Rotation
-	position.y += (cos(time * 5) * 1) * delta # Sine movement
+	position.y += (cos(time * 5) * 0.5) * delta # Sine movement
 
 	time += delta
