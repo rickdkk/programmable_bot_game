0499a84f8249ac5c9fd2ded35563a1fe335269ca
rickdkk <r.de.klerk@pl.hanze.nl>
2023-11-15 14:43:09 +0100

    Add multiple levels

---
+++ levels/level.gd
@@ -0,0 +1,45 @@
+extends Node3D
+class_name Level
+
+signal finished
+signal player_died
+
+@onready var victory_sound := $VictorySound as AudioStreamPlayer
+@onready var lose_sound := $LoseSound as AudioStreamPlayer
+@onready var player := $Player as Player
+
+enum MOVE_SET {FORWARD, BACK, LEFT, RIGHT}
+
+var moves := {MOVE_SET.FORWARD: Vector3.FORWARD, MOVE_SET.BACK: Vector3.BACK,
+			  MOVE_SET.LEFT: Vector3.LEFT, MOVE_SET.RIGHT: Vector3.RIGHT}
+var level_finished := false
+
+
+func _on_flag_body_entered(body: Node3D) -> void:
+	level_finished = true
+	if body.name == "Player":
+		victory_sound.play()
+	await victory_sound.finished
+	finished.emit()
+
+
+func _on_hud_go_button_pressed(current_actions: Array[MOVE_SET]) -> void:
+	var move_sequence: Array[Vector3] = []
+	for action in current_actions:
+		move_sequence.append(moves[action])
+	player.add_sequence(move_sequence)
+
+
+func _on_player_sequence_finished() -> void:
+	_lose()
+
+
+func _on_player_player_died() -> void:
+	_lose()
+
+func _lose() -> void:
+	if level_finished:
+		return
+	lose_sound.play()
+	await lose_sound.finished
+	player_died.emit()
--- main.gd
+++ main.gd
@@ -1,38 +1,33 @@
-extends Node3D
+extends Node
 
-@onready var victory_sound := $VictorySound as AudioStreamPlayer
-@onready var lose_sound := $LoseSound as AudioStreamPlayer
-@onready var player := $Player as Player
-
-enum MOVE_SET {FORWARD, BACK, LEFT, RIGHT}
-
-var moves := {MOVE_SET.FORWARD: Vector3.FORWARD, MOVE_SET.BACK: Vector3.BACK,
-			  MOVE_SET.LEFT: Vector3.LEFT, MOVE_SET.RIGHT: Vector3.RIGHT}
+var levels := [preload("res://levels/level_01.tscn"),
+			   preload("res://levels/level_02.tscn"),
+			   preload("res://levels/level_03.tscn")]
+var current_level: Level
+var current_level_scene
 
 
-func _on_flag_body_entered(body: Node3D) -> void:
-	if body.name == "Player":
-		victory_sound.play()
-	await victory_sound.finished
-	get_tree().reload_current_scene()
+func _ready() -> void:
+	load_next_level()
 
 
-func _on_hud_go_button_pressed(current_actions: Array[MOVE_SET]) -> void:
-	var move_sequence: Array[Vector3] = []
-	for action in current_actions:
-		move_sequence.append(moves[action])
-	player.add_sequence(move_sequence)
+func load_next_level():
+	if not levels.size():
+		return
+	if current_level:
+		remove_child(current_level)
+
+	current_level_scene = levels.pop_front()
+	load_level(current_level_scene)
 
 
-func _on_player_sequence_finished() -> void:
-	lose_sound.play()
-	await lose_sound.finished
-	get_tree().reload_current_scene()
-	print("here")
+func reload_level():
+	remove_child(current_level)
+	load_level(current_level_scene)
 
 
-func _on_player_player_died() -> void:
-	lose_sound.play()
-	await lose_sound.finished
-	get_tree().reload_current_scene()
-	print("there")
+func load_level(scene: PackedScene):
+	current_level = scene.instantiate()
+	current_level.finished.connect(load_next_level)
+	current_level.player_died.connect(reload_level)
+	add_child(current_level)
